a23f37cb72fb4a2b5d1507237758e1592e597edc
Keshav Kumar <anil.apex@outlook.com>
2022-06-19 17:32:49 +0530

    Quotes: Login

--- AhmedRafik/spiders/StaticLogin.py
+++ AhmedRafik/spiders/StaticLogin.py
@@ -1,11 +1,34 @@
 from scrapy import Spider
+from scrapy.http.request import Request
+from scrapy.http.request.form import FormRequest
 from scrapy.http.response.html import HtmlResponse
 
 
 class StaticLoginSpider(Spider):
     name = 'StaticLogin'
     allowed_domains = ['quotes.toscrape.com']
-    start_urls = ['http://quotes.toscrape.com']
 
-    def parse(self, response:HtmlResponse):
-        self.logger.info(f"User-Agent: {response.request.headers['User-Agent']}")
+    def start_requests(self):
+        yield Request(url="http://quotes.toscrape.com/login", callback=self.Login)
+
+    def Login(self, response: HtmlResponse):
+        formXpath = "//form[@action='/login']"
+        payload = \
+            {
+                "csrf_token": response.xpath("//input[@name='csrf_token']/@value").get(),
+                "username": "admin",
+                "password": "password"
+            }
+        yield FormRequest.from_response(response=response, formxpath=formXpath, formdata=payload,
+                                        callback=self.Parse, clickdata={"value": "Login"})
+
+    def Parse(self, response: HtmlResponse):
+        if not self.IsAuthenticated(response):
+            self.logger.error("Login failed!!!")
+            return
+
+        self.logger.info("Logged in successfully :)")
+
+    def IsAuthenticated(self, response) -> bool:
+        logOut = response.xpath("//a[text()='Logout']")
+        return True if logOut else False
